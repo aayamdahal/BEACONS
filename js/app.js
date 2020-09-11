012af13bf631f920a67a96fb148030ef9d8b3188
aayamdahal <imdahal1@gmail.com>
2020-09-11 20:23:21 +0545

    fifth-push

--- js/app.js
+++ js/app.js
@@ -163,7 +163,7 @@ function show_hide(){
     var click = document.getElementById('drop-content');
 
     if(click.style.display ==="none"){
-        click.style.display = "block"
+        click.style.display = "inline-block";
 
     } else { 
         click.style.display = "none";
